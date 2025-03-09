5819c420570bbeaae737d5833612407a1240af66
sergiosantosjr94 <sllsjunior@gmail.com>
2025-03-09 16:09:42 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,45 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
-  return ( 
-  <div className="grid grid-cols-2 h-full">
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
+  return (
+    <div className="grid grid-cols-2 h-full">
       {/* Left Position */}
       <div className="flex h-full flex-col justify-center p-8 max-w-[550px] mx-auto">
-
-      <Image src="/logo.svg" width={173} height={39} alt='Finance AI' className="mb-8" />
-      <h1 className="text-4xl font-bold mb-3">Welcome!</h1>
-      <p className="text-muted-foreground mb-8">Finance AI is a finance management plataform develop by Sérgio Santos Jr, that uses AI for monitoring your movimentations, and offers tailored insights faciliting the control of your budget.  </p>
-      <Button variant="outline">
-         <LogInIcon />
-         Login or Signup
-      </Button>
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
+        <h1 className="text-4xl font-bold mb-3">Welcome!</h1>
+        <p className="text-muted-foreground mb-8">
+          Finance AI is a finance management plataform develop by Sérgio Santos
+          Jr, that uses AI for monitoring your movimentations, and offers
+          tailored insights faciliting the control of your budget.
+        </p>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon />
+            Login or Signup
+          </Button>
+        </SignInButton>
       </div>
       {/* Right Position */}
       <div className="relative h-full w-full">
-         <Image src="/login.png" alt="Login" fill className="object-cover"/>
+        <Image src="/login.png" alt="Login" fill className="object-cover" />
       </div>
-   </div>
-   );
-}
- 
+    </div>
+  );
+};
+
 export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
-import { Button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-export default function Home() {
-  return <Button>Hello World</Button>
-          
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 }
